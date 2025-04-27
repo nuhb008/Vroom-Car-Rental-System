6ef5831b8246d35258c8451ae4dd71d084025310
iraiyan50 <iraiyan50@gmail.com>
2025-04-27 14:55:36 +0600

    initialized fullstack architecture

---
+++ vroom/src/test/java/com/trident/vroom/VroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trident.vroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
