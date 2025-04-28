31424b6656c63a04aab3259c212fc3f6204e4019
ziftikhr <ziftikhr@gmail.com>
2025-04-29 00:18:55 +0600

    updated UI for Admin

---
+++ vroom-front/src/pages/AdminFunctions/CheckNewCarApplication.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckNewCarApplication() {
+  return (
+    <div>
+      <h2>Check New Car Application </h2>
+    </div>
+  )
+}
+
+export default CheckNewCarApplication
