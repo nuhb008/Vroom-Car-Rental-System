c2ea4e5d10a8ab312b857f89d46dc80798ec4f5d
Nuh Islam <nuhislam@iut-dhaka.edu>
2025-04-29 17:27:13 +0600

    payment update

--- vroom-front/src/services/api.js
+++ vroom-front/src/services/api.js
@@ -23,6 +23,7 @@ export const getAllBookings = async () => axios.get(`${API_URL}/bookings`);
 export const createBooking = async (booking) => axios.post(`${API_URL}/bookings`, booking);
 export const updateBooking = async (id, booking) => axios.put(`${API_URL}/bookings/${id}`, booking);
 
+
 export const getAllPayments = async () => axios.get(`${API_URL}/payments`);
 export const getPaymentById = async (id) => axios.get(`${API_URL}/payments/${id}`);
 export const createPayment = async (payment) => axios.post(`${API_URL}/payments`, payment);
@@ -31,3 +32,13 @@ export const deletePayment = async (id) => axios.delete(`${API_URL}/payments/${i
 export const getPaymentsByRentId = async (rentId) => axios.get(`${API_URL}/payments/rent/${rentId}`);
 export const getPaymentsByDateRange = async (startDate, endDate) =>axios.get(`${API_URL}/payments/daterange?start=${startDate}&end=${endDate}`);
 export const getPaymentsByStatus = async (status) => axios.get(`${API_URL}/payments/status/${status}`);
+
+
+// Rentals API
+export const getAllRentals = async () => axios.get(`${API_URL}/rentals`);
+export const getRentalById = async (id) => axios.get(`${API_URL}/rentals/${id}`);
+export const createRental = async (rental) => axios.post(`${API_URL}/rentals`, rental);
+export const updateRental = async (id, rental) => axios.put(`${API_URL}/rentals/${id}`, rental);
+export const deleteRental = async (id) => axios.delete(`${API_URL}/rentals/${id}`);
+export const getRentalsByStatus = async (status) => axios.get(`${API_URL}/rentals/status/${status}`);
+export const getRentalsByCustomerId = async (customerId) => axios.get(`${API_URL}/rentals/customer/${customerId}`);
